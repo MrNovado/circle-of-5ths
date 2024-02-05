fdec91f31966fdb9617fa15cd028543f5692cfc3
mrnovado <novado@ymail.com>
2024-02-05 19:24:24 +0500

    basic canvas

---
+++ src/lib/Circle.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  import { onMount } from "svelte";
+
+  let canvas: HTMLCanvasElement;
+
+  onMount(() => {
+    const ctx = canvas.getContext("2d")!;
+    ctx.fillRect(0, 0, 100, 100);
+  });
+</script>
+
+<canvas bind:this={canvas} />
+
+<style>
+  canvas {
+    width: 100%;
+    height: 100%;
+    /* background-color: #666; */
+  }
+</style>
