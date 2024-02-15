5acbc1ccbcefabdb5fb20d3b966022ebb9c18205
mrnovado <novado@ymail.com>
2024-02-15 18:09:49 +0500

    review

--- src/lib/common/index.ts
+++ src/lib/common/index.ts
@@ -1,23 +1,30 @@
 export type ChordsSeq = [string, string, string][];
-export type ChordDesc = { ch: string; rId?: number; gi: number };
+export type ChordDesc = { ch: string; rId?: number; chGI: number };
 export type ChordsSeqExp = [ChordDesc, ChordDesc, ChordDesc][];
-export type ChordItem = { v: ChordsSeqExp[number]; n: number; p: number };
+export type ChordItem = {
+  v: ChordsSeqExp[number];
+  n: number;
+  p: number;
+  sGI: number;
+};
+
 export type ChordsRec = Record<number, ChordItem>;
 
 export const CURRENT_SEQ = [0, 1, 11];
 
 export const seqToRec = (seq: ChordsSeq | ChordsSeqExp) => {
-  let gi = -1;
+  let chGI = -1;
   return seq.reduce(
     (acc, v, seqInd, a) => {
       acc[seqInd] = {
         v: v.map((el) => {
-          gi += 1;
+          chGI += 1;
           return typeof el === "string" //
-            ? { ch: el, gi }
-            : { ...el, gi };
+            ? { ch: el, chGI }
+            : { ...el, chGI };
         }) as ChordsSeqExp[number], //
 
+        sGI: seqInd,
         n: seqInd === a.length - 1 ? 0 : seqInd + 1,
         p: seqInd === 0 ? a.length - 1 : seqInd - 1,
       };
