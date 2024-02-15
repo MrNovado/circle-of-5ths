0f5b67d97b59f233e1ee4ad39927bb2651daf8bc
mrnovado <novado@ymail.com>
2024-02-15 18:06:51 +0500

    gi index for each chord fix

--- src/lib/common/index.ts
+++ src/lib/common/index.ts
@@ -1,24 +1,31 @@
 export type ChordsSeq = [string, string, string][];
-export type ChordDesc = { ch: string; rId?: number };
+export type ChordDesc = { ch: string; rId?: number; gi: number };
 export type ChordsSeqExp = [ChordDesc, ChordDesc, ChordDesc][];
-export type ChordItem = { v: ChordsSeqExp[number]; gi: number; n: number; p: number };
+export type ChordItem = { v: ChordsSeqExp[number]; n: number; p: number };
 export type ChordsRec = Record<number, ChordItem>;
 
 export const CURRENT_SEQ = [0, 1, 11];
 
-export const seqToRec = (seq: ChordsSeq | ChordsSeqExp) =>
-  seq.reduce(
-    (acc, v, index, a) => {
-      acc[index] = {
-        v: v.map((s) => (typeof s === "string" ? { ch: s } : s)) as ChordsSeqExp[number], //
-        gi: index,
-        n: index === a.length - 1 ? 0 : index + 1,
-        p: index === 0 ? a.length - 1 : index - 1,
+export const seqToRec = (seq: ChordsSeq | ChordsSeqExp) => {
+  let gi = -1;
+  return seq.reduce(
+    (acc, v, seqInd, a) => {
+      acc[seqInd] = {
+        v: v.map((el) => {
+          gi += 1;
+          return typeof el === "string" //
+            ? { ch: el, gi }
+            : { ...el, gi };
+        }) as ChordsSeqExp[number], //
+
+        n: seqInd === a.length - 1 ? 0 : seqInd + 1,
+        p: seqInd === 0 ? a.length - 1 : seqInd - 1,
       };
       return acc;
     }, //
-    {} as ChordsRec
+    {} as ChordsRec,
   );
+};
 
 export const SEQ_FILLER = new Array(12).fill(0);
 
